66c129ada340d8a8bf64d50ca790b7ffd28b3894
neuvecom <neuvecom@gmail.com>
2024-02-18 19:00:56 +0900

    ゲームループの作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::time::SystemTime;
+use std::thread::sleep;
+use std::time::Duration;
+
 const COAT_SIZE: i32 = 64;
 
 // 描画処理
@@ -13,9 +17,25 @@ fn draw() {
     println!("\x1B[1;1H{}", buf);
 }
 
+// ループ処理
+fn game_loop() {
+    // 時間の初期化
+    let mut time: SystemTime = SystemTime::now();
+    // ループ
+    loop{
+        // 描画処理
+        draw();
+        // 時間処理
+        time += Duration::from_nanos(16_666_667);
+        if let Ok(dur) = time.duration_since(SystemTime::now()) {
+            sleep(dur);
+        }
+    }
+}
+
 fn main() {
     // 画面のクリア
     println!("\x1B[2J");
-    // 描画処理
-    draw();
+    // ゲームループ処理
+    game_loop();
 }
